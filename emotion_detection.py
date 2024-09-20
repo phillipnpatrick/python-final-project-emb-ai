cb147c3a0d18d29fef040a623799859e7f564469
Phillip Patrick <phillip.n.patrick@gmail.com>
2024-09-20 19:43:52 -0400

    task 3 - format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -14,4 +14,49 @@ def emotion_detector(text_to_analyse):
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
 
-    return response.text  # Return the response text from the API
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        dominant_emotion = "anger"
+        dominant_score = anger_score
+
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        if (disgust_score > dominant_score):
+            dominant_emotion = "disgust"
+            dominant_score = disgust_score
+
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        if (fear_score > dominant_score):
+            dominant_emotion = "fear"
+            dominant_score = fear_score
+
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        if (joy_score > dominant_score):
+            dominant_emotion = "joy"
+            dominant_score = joy_score
+
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        if (sadness_score > dominant_score):
+            dominant_emotion = "sadness"
+            dominant_score = sadness_score
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 500:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
+
+    # Return the label and score in a dictionary
+    return {
+        'anger': anger_score, 
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
